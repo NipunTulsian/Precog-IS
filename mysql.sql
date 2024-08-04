715724c7a7b205e2da26a70995e47a2195e7e020
Nipun Tulsian <nipuntulsian@Nipuns-Laptop.local>
2024-08-04 16:51:53 +0530

    adding sql

---
+++ mysql.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 10.13  Distrib 8.2.0, for macos14.0 (arm64)
+--
+-- Host: localhost    Database: precog
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'nipun.tulsian.nt@gmail.com','abc','nipun','tulsian','admin'),(2,'nipun.tulsian@gmail.com','abc','nipun','tulsian','admin');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `meet_details`
+--
+
+DROP TABLE IF EXISTS `meet_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meet_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mentee_id` int DEFAULT NULL,
+  `mentor_id` int DEFAULT NULL,
+  `affiliation` text NOT NULL,
+  `research_problem` text NOT NULL,
+  `feedback` text NOT NULL,
+  `times_before` varchar(256) NOT NULL,
+  `date_of_meet` date DEFAULT NULL,
+  `time_of_meet` time DEFAULT NULL,
+  `status` varchar(256) NOT NULL,
+  `slot_index` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `mentee_id` (`mentee_id`),
+  KEY `mentor_id` (`mentor_id`),
+  CONSTRAINT `meet_details_ibfk_1` FOREIGN KEY (`mentee_id`) REFERENCES `mentee` (`id`),
+  CONSTRAINT `meet_details_ibfk_2` FOREIGN KEY (`mentor_id`) REFERENCES `mentor` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `meet_details`
+--
+
+LOCK TABLES `meet_details` WRITE;
+/*!40000 ALTER TABLE `meet_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `meet_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mentee`
+--
+
+DROP TABLE IF EXISTS `mentee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mentee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `registration_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mentee`
+--
+
+LOCK TABLES `mentee` WRITE;
+/*!40000 ALTER TABLE `mentee` DISABLE KEYS */;
+INSERT INTO `mentee` VALUES (2,'neelamvkt@gmail.com','scrypt:32768:8:1$3Q9AtFFV7LlhPVEJ$2500b3c1d38d827d78dc3523a5eaca8d49226ec46becfaa2f9a66cd04893affedb5499ebd555e30f7db28af3cf2f19ef5c4a1a08dca9e0883af3aeed45a3760d','nilam','tulsian','mentee','2024-08-03 10:54:01');
+/*!40000 ALTER TABLE `mentee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mentor`
+--
+
+DROP TABLE IF EXISTS `mentor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mentor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `Description` text,
+  `slots` text,
+  `meet_link` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mentor`
+--
+
+LOCK TABLES `mentor` WRITE;
+/*!40000 ALTER TABLE `mentor` DISABLE KEYS */;
+INSERT INTO `mentor` VALUES (7,'nipun.tulsian.nt@gmail.com','abc','Dr. Sunpreet','Arora','mentor','Visa - Biometrics, Machine Learning, Systems Security','4','ergvdfwf'),(8,'nipun.tulsian.nt@gmail.com','abc','Dr. Siddhartha','Asthana','mentor','MasterCard - Human Computer Interaction/ML',NULL,NULL),(9,'nipun.tulsian.nt@gmail.com','abc','Dr. Arun','Babu','mentor','ARTPARK/IISc - Software Engineering, Cybersecurity',NULL,NULL);
+/*!40000 ALTER TABLE `mentor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `otp_table`
+--
+
+DROP TABLE IF EXISTS `otp_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `otp_table` (
+  `email` varchar(255) NOT NULL,
+  `otp` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `otp_table`
+--
+
+LOCK TABLES `otp_table` WRITE;
+/*!40000 ALTER TABLE `otp_table` DISABLE KEYS */;
+INSERT INTO `otp_table` VALUES ('nipun.tulsian@students.ac.in','619580');
+/*!40000 ALTER TABLE `otp_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-04 16:51:24
